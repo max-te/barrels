d1b805fe5be7574076746c608ee2370d67566de8
Max Teegen <m_teegen@gmx.de>
2024-11-27 21:15:22 +0100

    fixes: add missing env.sh, update gitignore

---
+++ env.sh
@@ -0,0 +1,7 @@
+HERE=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+
+export LD_LIBRARY_PATH="${HERE}/lib:${LD_LIBRARY_PATH:-}" \
+    PATH="${HERE}/bin:${PATH}" \
+    WINEPREFIX="${HERE}/prefix" \
+    WINEARCH="win64" \
+    WINEDLLOVERRIDES="mscoree,mshtml="
